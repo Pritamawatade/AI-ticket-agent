2a8bb6263b1cb73e6e19685e4e9baf7a833f74a2
pritam <pritamawatade5@gmail.com>
2025-08-07 12:48:12 +0530

    feat: added mongoose connection added start dev and test scripts

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
+const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT);
+const MONGO_URI =process.env.MONGO_URI || "mongodb://localhost:27017/ai-ticket";
+const app = express();
+console.log(MONGO_URI);
+app.use(cors());
+app.use(express.json());
+mongoose
+.connect(MONGO_URI)
+.then(() => {
+      console.log("Starting server");
+    console.log("Connected to MongoDB");
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
