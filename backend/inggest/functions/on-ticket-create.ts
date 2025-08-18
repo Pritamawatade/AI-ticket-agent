edbff886c2af6455c7a9098a96dd4495599c6cd4
Pritamawatade <pritamawatade5@gmail.com>
2025-08-18 18:35:30 +0530

    ticket analyzer AI pipeline complete

---
+++ backend/inggest/functions/on-ticket-create.ts
@@ -0,0 +1,50 @@
+import { NonRetriableError } from "inngest";
+import User, { type IUSER as UserModel } from "../../models/user";
+import { inngest } from "../client";
+import { sendMail } from "../../libs/mailer";
+import Ticket, { type ITicket } from "../../models/ticket";
+import analyzeTicket from "../../libs/ai";
+
+export const onTicketCreate = inngest.createFunction(
+    { id: "on-ticket-create", retries: 2 },
+    { event: "ticket/create" },
+    async ({ event, step }) => {
+        try {
+            const { ticketId } = event.data;
+
+            const ticket: any = await step.run("get-ticket", async () => {
+                const ticket = await Ticket.findById(ticketId);
+                if (!ticket) {
+                    throw new NonRetriableError("Ticket not found");
+                }
+
+                return ticket;
+            });
+
+            await step.run("update-ticket-status", async () => {
+                await Ticket.findByIdAndUpdate(ticket._id, { status: "TODO" });
+            });
+
+            const aiResponse = await analyzeTicket(ticket);
+
+            const relatedSkills = await step.run("ai-processing", async () => {
+                let skills = [];
+
+                if (aiResponse) {
+                    await Ticket.findByIdAndUpdate(ticket._id, {
+                        priority: !["low", "medium", "high"].includes(aiResponse.priority)
+                            ? "medium"
+                            : aiResponse.priority,
+                        helpfulNotes: aiResponse.helpfulNotes,
+                        relatedSkills: aiResponse.relatedSkills,
+                        status: "IN_PROGRESS",
+                    });
+                    skills = aiResponse.relatedSkills;
+                }
+                return skills;
+            });
+        } catch (error) {
+            console.error(`❌ Error in onTicketCreate function`, error);
+        }
+    }
+);
--- backend/libs/ai.ts
+++ backend/libs/ai.ts
@@ -25,7 +25,7 @@ const analyzeTicket = async (ticket: any) => {
         `,
     });
 
-    const response =
+    const response: any =
         await supportAgent.run(`You are a ticket triage agent. Only return a strict JSON object with no extra text, headers, or markdown.
         
 Analyze the following support ticket and provide a JSON object with:
@@ -50,4 +50,17 @@ Ticket information:
 
 - Title: ${ticket.title}
 - Description: ${ticket.description}`);
+
+    const raw = response.output[0].context;
+
+    try {
+        const match = raw.match(/```json\s*([\s\S]*?)\s*```/i);
+        const jsonString = match ? match[1] : raw.trim();
+        return JSON.parse(jsonString);
+    } catch (e: any) {
+        console.log("Failed to parse JSON from AI response" + e.message);
+        return null; // watch out for this
+    }
 };
+
+export default analyzeTicket;
