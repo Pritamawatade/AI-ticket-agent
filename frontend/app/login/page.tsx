3d47d95bbe9428d94b0d211293a6431904225887
Pritamawatade <pritamawatade5@gmail.com>
2025-08-27 12:54:58 +0530

    feat: login mechanism done and tested

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,20 +3,37 @@ import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import axios from "axios"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function Login() {
     const [form, setForm] = useState({ email: "", password: "" })
-
+    const [loading, setLoading] = useState(false)
+    const navigate = useRouter()
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
-    const login = async (data: { email: string; password: string }) => {
+    const login = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true)
         try {
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/login`, form)
+
+            if (res.status === 200) {
+                const { user, token } = res.data
+
+                localStorage.setItem("token", token)
+                navigate.push("/")
+            } else {
+                setLoading(false)
+                alert(res.data.error)
+            }
         } catch (error: any) {
             console.log(`something went wrong at logind ${error}`)
+            setLoading(false)
         }
     }
     return (
@@ -32,11 +49,19 @@ export default function Login() {
                     </CardAction>
                 </CardHeader>
                 <CardContent>
-                    <form>
+                    <form onSubmit={login}>
                         <div className="flex flex-col gap-6">
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="m@example.com" required />
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    value={form.email}
+                                    onChange={handleChange}
+                                    required
+                                />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
@@ -48,15 +73,22 @@ export default function Login() {
                                         Forgot your password?
                                     </a>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    value={form.password}
+                                    onChange={handleChange}
+                                    type="password"
+                                    required
+                                />
                             </div>
                         </div>
+                        <Button type="submit" className="w-full mt-8" disabled={loading}>
+                            Login
+                        </Button>
                     </form>
                 </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
+                <CardFooter className="flex-col gap-2 mt-[-11px]">
                     <Button variant="outline" className="w-full">
                         Login with Google
                     </Button>
